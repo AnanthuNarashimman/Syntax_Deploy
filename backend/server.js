7992e10d1a9d62dd0423d2842f6abc4ee74c6ec0
AnanthuNarashimman <ananthu.narashimman@gmail.com>
2026-01-16 18:29:59 +0530

    Updated origin

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: [
-      process.env.FRONTEND_URL,
-      "http://localhost:5173"
-    ],
+    origin: 
+      process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
