1ef0edf99023d45120a6f6b32766d84fa52e21f9
AnanthuNarashimman <ananthu.narashimman@gmail.com>
2026-01-16 21:39:39 +0530

    include credentials

--- syntax/src/Pages/SuperAdminLogPage.jsx
+++ syntax/src/Pages/SuperAdminLogPage.jsx
@@ -58,6 +58,7 @@ function SuperAdminLogPage() {
                 headers: {
                     'Content-Type': 'application/json',
                 },
+                credentials: 'include',
                 body: JSON.stringify(formData),
             });
 
