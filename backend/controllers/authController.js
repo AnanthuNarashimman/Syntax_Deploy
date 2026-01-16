dbaf039cd98151b197d92992dbec36b6ec23acf4
AnanthuNarashimman <ananthu.narashimman@gmail.com>
2026-01-16 20:02:02 +0530

    removed partitioning

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -17,7 +17,6 @@ const adminLogin = async (req, res) => {
       secure: true,
       maxAge: 1000 * 60 * 180, // 3 hours
       sameSite: "None",
-      partitioned: true,
       path: "/",
     });
 
